45a6390886f5c926c2b74de7d8c48915600b168a
Andrew Zures <azures@8thlight.com>
2015-02-21 11:01:10 -0600

    continues calculator module

--- stanford/Calculator/Calculator/Calculator.swift
+++ stanford/Calculator/Calculator/Calculator.swift
@@ -5,22 +5,43 @@ public class Calculator {
     public init(){}
     
     public func add(stack: [Double]) -> (Double?, [Double]){
-        return execute(stack, { $0 + $1 })
+        return operate(stack, { $0 + $1 })
     }
     
     public func multiply(stack: [Double]) -> (Double?, [Double]){
-        return execute(stack, { $0 * $1 })
+        return operate(stack, { $0 * $1 })
     }
     
     public func divide(stack: [Double]) -> (Double?, [Double]){
-        return execute(stack, { $1 / $0 })
+        return operate(stack, { $1 / $0 })
     }
     
     public func subtract(stack: [Double]) -> (Double?, [Double]){
-        return execute(stack, { $1 - $0 })
+        return operate(stack, { $1 - $0 })
     }
     
-    func execute(var stack: [Double], operand: ((Double, Double) -> Double)) -> (Double?, [Double]){
+    public func sqrt(stack: [Double]) -> (Double?, [Double]){
+        return operate(stack, { Darwin.sqrt($0) })
+    }
+    
+    public func sin(stack: [Double]) -> (Double?, [Double]){
+        return operate(stack, { Darwin.sin($0) })
+    }
+    
+    public func cos(stack: [Double]) -> (Double?, [Double]){
+        return operate(stack, { Darwin.cos($0) })
+    }
+    
+    func operate(var stack: [Double], operand: ((Double) -> Double)) -> (Double?, [Double]){
+        if stack.count < 1 { return (nil, stack) }
+        
+        let calculated = operand(stack.removeLast())
+        
+        stack.append(calculated)
+        return (calculated, stack)
+    }
+    
+    func operate(var stack: [Double], operand: ((Double, Double) -> Double)) -> (Double?, [Double]){
         if stack.count < 2 { return (nil, stack) }
         
         let calculated = operand(stack.removeLast(), stack.removeLast())
--- stanford/Calculator/CalculatorTests/CalculatorTest.swift
+++ stanford/Calculator/CalculatorTests/CalculatorTest.swift
@@ -4,27 +4,53 @@ import Calculator
 class CalculatorTest: XCTestCase {
 
     func testAddsAndReturnCorrectStack() {
-        var (result, rest) = Calculator().add([1,2,5])
+        let (result, rest) = Calculator().add([1,2,5])
+        
         XCTAssertEqual(7, result!)
         XCTAssertEqual([1,7], rest)
     }
     
     func testSubtractsAndsReturnsCorrectStack() {
-        var (result, rest) = Calculator().subtract([1,5,2])
+        let (result, rest) = Calculator().subtract([1,5,2])
+        
         XCTAssertEqual(3, result!)
         XCTAssertEqual([1,3], rest)
     }
     
     func testDividesAndsReturnsCorrectStack() {
-        var (result, rest) = Calculator().divide([1,6,2])
+        let (result, rest) = Calculator().divide([1,6,2])
+        
         XCTAssertEqual(3, result!)
         XCTAssertEqual([1,3], rest)
     }
     
     func testMultipliesAndsReturnsCorrectStack() {
-        var (result, rest) = Calculator().multiply([1,3,2])
+        let (result, rest) = Calculator().multiply([1,3,2])
+        
         XCTAssertEqual(6, result!)
         XCTAssertEqual([1,6], rest)
+    }
+    
+    func testSqrtAndReturnsCorrectStack(){
+        let (result, rest) = Calculator().sqrt([1,3,16])
+        XCTAssertEqual(4, result!)
+        XCTAssertEqual([1,3,4], rest)
+    }
+    
+    func testSinAndReturnsCorrectStack(){
+        let sinOfTwo = 0.90929742682
+        let (result, rest) = Calculator().sin([1,3,2])
+        
+        XCTAssertEqualWithAccuracy(sinOfTwo, result!, 0.00001)
+        XCTAssertEqualWithAccuracy([1,3, sinOfTwo].reduce(0,+), rest.reduce(0,+), 0.00001)
+    }
+    
+    func testCosAndReturnsCorrectStack(){
+        let cosOfTwo = -0.41614683654
+        let (result, rest) = Calculator().cos([1,3,2])
+        
+        XCTAssertEqualWithAccuracy(cosOfTwo, result!, 0.00001)
+        XCTAssertEqualWithAccuracy([1,3, cosOfTwo].reduce(0,+), rest.reduce(0,+), 0.00001)
     }
 
 }
